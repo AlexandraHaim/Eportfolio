d62ef90746a4d06e4ef28536e5e4b2ecd6ce39e3
AlexandraHaim <alexandra.haim@hotmail.fr>
2022-07-03 17:49:32 +0200

    start imses 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import styled from "styled-components";
-import aiimage from "../../utils/data/images/ai.png"
+// import styled from "styled-components";
+// import aiimage from "../../utils/data/images/ai.png"
 
-const Image = styled.img`
-  width: 100%;
-  height: 170px;
-`
+// const Image = styled.img`
+//   width: 100%;
+//   height: 170px;
+// `
 export default function Header() {
   return (
     <div>
